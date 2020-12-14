1ac0518a65abc9fa5759d63c29a2d1f7cbc5b90a
andyseracuse <andrew.seracuse@gmail.com>
2020-12-13 17:32:11 -0700

    solved quicksort

---
+++ quicksort.js
@@ -0,0 +1,16 @@
+const quicksort = function(arr) {
+  if (arr.length <= 1) {
+    return arr
+  }
+  const pivot = arr[0]
+  let left = [];
+  let right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > pivot) {
+      right.push(arr[i])
+    } else {
+      left.push(arr[i])
+    }
+  }
+  return [...quicksort(left), pivot, ...quicksort(right)]
+}
